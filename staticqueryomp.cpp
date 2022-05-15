84b7fdae1e13b9ac3d2b601365883f3e4e126b96
杨万里 <1194970401@qq.com>
2022-05-15 21:33:33 +0800

    以查询为单位的任务划分（OpenMP）

--- staticqueryomp.cpp
+++ staticqueryomp.cpp
@@ -5,12 +5,10 @@
 #include<iostream>
 #include<string>
 #include<bitset>
-#include<sys/time.h>
 #include<arm_neon.h>
 #ifdef _OPENMP
-#include<omp.h>
-#endif // _OPENMP
-#pragma warning( disable : 4996) 
+#include<omp>
+#endif // 
 
 using namespace std;
 FILE *fi;
@@ -43,10 +41,7 @@ vector<int> strtoints(char* line) {
 }
 
 int main() {
-	struct timeval t1, t2;
-	double timeuse = 0;
 	//打开文档读入数据集
-	
 	fi = fopen("/home/s2013774/OpenMP/ExpIndex", "rb");
 	if (NULL == fi) {
 		printf("Can not open file ExpIndex!\n");
@@ -94,35 +89,38 @@ int main() {
 		query_list.push_back(arr);
 	}
 	fclose(fp);
+
 	//接下来开始实现倒排索引求交技术
 	//实现按表求交的位图存储方法
-	int QueryNum = 100;//查询次数
+	int QueryNum = 1000;//查询次数
+#pragma omp parallel for num_threads(5) 
 	for (int i = 0;i < QueryNum;i++) {
 		int TermNum = query_list[i].size();
 		bitset<25214976> * lists;//25214976=128*196992
 		lists = new bitset<25214976>[TermNum];
 		bitset<196992> *  second;
 		second = new bitset<196992>[TermNum];
-#pragma omp parallel for num_threads(TermNum)
 		for (int j = 0;j < TermNum;j++) {
 			for (int k = 0;k < idx[query_list[i][j]].len;k++) {
 				lists[j].set(idx[query_list[i][j]].arr[k]);//括号内是文档编号，把文档对应的二进制位置为1
 			}
+		}
 		//第一层的位向量已经存储完毕，接下来建立索引,建立索引已经得到了优化
-			long addrtemp1 = (long)&lists[j];
-			for (int k = 0;k < 196992;k++) {
-				bitset<128> * setptr = (bitset<128>*)(addrtemp1 + 16 * k);
+		for (int i = 0;i < TermNum;i++) {
+			long addrtemp1 = (long)&lists[i];
+			for (int j = 0;j < 196992;j++) {
+				bitset<128> * setptr = (bitset<128>*)(addrtemp1 + 16 * j);
 				if (*setptr == 0) {
 					;
 				}
 				else {
-					second[j].set(k);
+					second[i].set(j);
 				}
 			}
 		}
+
 		for (int i = 1;i < TermNum;i++) {
 			second[0] &= second[i];  //二级索引层直接按位与，底层SIMD
-#pragma omp parallel for num_threads(8) schedule(dynamic,1539)
 			for (int j = 0;j < 196992;j++) {
 				if (second[0][j] == 1) {  //第j位是1，需要底层求交
 					long addrtemp1 = (long)&lists[0];
@@ -142,12 +140,8 @@ int main() {
 			}
 		}
 		vector<unsigned int> result;
-		//vector<unsigned int> myresult[8];
-		vector<unsignd int> myresult
 		//结果转换优化，list[0]就是结果位向量，256为单位判断
-		gettimeofday(&t1, NULL);
 		long address = (long)&lists[0];
-#pragma omp parallel for num_threads(8) private(myresult)
 		for (int j = 0;j < 98496;j++) {
 			bitset<256> * setptr = (bitset<256>*)(address + 32 * j);
 			if (*setptr == 0) {
@@ -157,23 +151,15 @@ int main() {
 			else {
 				for (int k = 0;k < 256;k++) {
 					if (lists[0][j * 256 + k] == 1) {
-						myresult.push_back(j * 256 + k);
+						result.push_back(j * 256 + k);
 					}
 				}
 			}
 		}
-		//结果合并
-		for (int j = 0;j < 8;j++) {
-			result.insert(result.end(), myresult[j].begin(), myresult[j].end());
-		}
-		gettimeofday(&t2, NULL);
-		timeuse += (t2.tv_sec - t1.tv_sec) * 1000000 + t2.tv_usec - t1.tv_usec;
 		cout << result.size() << endl;
 	}
 	for (j = 0;j < n;j++) free(idx[j].arr);
 	free(idx);
-
-	cout << "time_use=" << timeuse << endl;
 
 	return 0;
 }
